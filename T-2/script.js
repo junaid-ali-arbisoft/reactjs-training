51966ca84a8173af7fcac337132b64f74672f30c
Syed Junaid Ali <junaid.ali@arbisoft.com>
2023-10-27 18:15:26 +0500

    T-2: To-do list

--- T-2/script.js
+++ T-2/script.js
@@ -1,9 +1,17 @@
 function getCurrentDate(){
     const date = new Date();
 
-    return date.toLocaleString();
+    return formatDate(date);
 }
 
+function formatDate(date) {
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    return `${day} ${month} ${year}`;
+  }
+  
 const dateLabel = document.getElementById("dateLabel")
 
 dateLabel.innerText = getCurrentDate();
